e1c6ca772bcb323876ecf4bc59316306e3b590c2
ushaganta <ganta.u@northeastern.edu>
2023-04-04 18:12:47 -0400

    Login and sign up react components and event reg component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import AuthForm from './login/AuthForm'
+import EventForm from './sellTicket/sellTicket'
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/*<AuthForm> </AuthForm>*/}
+     {/*   <EventForm></EventForm>;*/}
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/*"
+                           element={<AuthForm/>}/>
+                    <Route path="/event"
+                           element={<EventForm/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     </div>
   );
 }

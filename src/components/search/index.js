ac39eef127e4cb83c35030d5040c96c6183984fb
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-04-20 10:54:38 -0400

    Fixing search bug

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "../../Home/Navigation/index.js";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router";
@@ -9,40 +9,43 @@ const SearchComponent = () => {
     const location = useLocation();
     const queryParam = new URLSearchParams(location.search);
     const ename = queryParam.get("Ename");
-    const [search, setSearch] = useState(ename);
+    const [search, setSearch] = useState(queryParam.get("Ename"));
     const [results, setResults] = useState({});
     let navigate = useNavigate();
     const searchButtonHandler = () => {
         navigate('/search?Ename='+search);
     }
-    const searchEvent = async () => {
-        const response = await searchConcerts(ename);
-        setResults(response);
-    };
-    if (ename) {
-        searchEvent();
-    }
+    
+    useEffect(() => {
+        if (ename) {
+            const searchEvent = async () => {
+                const response = await searchConcerts(ename);
+                setResults(response);
+            };
+            searchEvent();
+        }
+    }, [ename]);
     return (
         <div className="container">
             <Navigation/>
             <div className="row">
                 <div className="input-group search-width">
                     <input className="form-control "
-                     type="text" placeholder="Search for concerts, sports, and more"
+                     type="text" value={search}
                      onChange={(event) => setSearch(event.target.value)}/>
                      <button className="btn btn-primary"
                         onClick={searchButtonHandler}
                      >Search</button>
                 </div>
                 
-                <div class=" d-flex justify-content-center">
+                <div className=" d-flex justify-content-center">
 
-                    <ul class="list-group  text-light">
+                    <ul className="list-group  text-light">
                 {results.length > 0 && results.map((result) => {
                     return (
-                        <li class="list-group-item  ">
+                        <li className="list-group-item  ">
                              <div className="row">
-                                <div class="col-2 mt-2 ">
+                                <div className="col-2 mt-2 ">
                                     <img src={result.Eimage}
                                     className="img-fluid rounded"
                                     alt={"event pic"}  />

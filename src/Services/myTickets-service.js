1078ee01e280aba908e846fb08ff828565025710
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-04-22 21:37:57 -0400

    Adding ENV var in services links

--- src/Services/myTickets-service.js
+++ src/Services/myTickets-service.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const loggedInTickets_API = "http://localhost:4000/api/tickets/user";
+// const loggedInTickets_API = "http://localhost:4000/api/tickets/user";
+const API_BASE = process.env.REACT_APP_API_BASE
+const loggedInTickets_API = `${API_BASE}/tickets/user`;
 
 export const loggedInUserTickets = async (userId) => {
   const response = await axios.get(`${loggedInTickets_API}/${userId}`);
